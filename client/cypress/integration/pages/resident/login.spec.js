b4ff0989bb112fe668c3796149afcfb3f7ca8c11
sntw-5569 <ore.no.yeah@gamil.com>
2022-03-15 09:33:54 +0900

    fix test

--- client/cypress/integration/pages/resident/login.spec.js
+++ client/cypress/integration/pages/resident/login.spec.js
@@ -9,4 +9,12 @@ describe('test resident login page', () => {
       'トップページ - 親子のメンタルヘルススクリーニング'
     )
   })
+
+  it('should login', () => {
+    cy.get('[data-cy=user-id]').type('resident-1')
+    cy.get('[data-cy=password]').type('resident0218')
+    cy.get('[data-cy=login-btn]').click()
+    cy.wait(3000)
+    cy.get('[data-cy=add-child-btn]').should('be.visible')
+  })
 })
